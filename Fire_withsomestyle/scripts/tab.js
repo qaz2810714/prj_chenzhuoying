a043f0f1aaf0cd6eb1b9df12414b66ba4b78b35c
shangyang <shangyang@youzan.com>
2021-11-14 18:30:02 +0800

    commit

---
+++ Fire_withsomestyle/scripts/tab.js
@@ -0,0 +1,10 @@
+$(function(e){
+	$(".tabgroup .tab").on("click", function(e){
+		let index = $(this).index();
+
+		$(this).addClass("active").siblings().removeClass("active");
+
+		$(this).closest(".tabgroup").find(".content").eq(index)
+		.addClass("active").siblings().removeClass("active");
+	});
+});
